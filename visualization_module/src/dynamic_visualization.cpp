82fee9b4a807557a9173f9d58c30ad580d6ccb70
LitoNeo <auto.hongda@gmail.com>
2019-03-26 22:47:45 +0800

    add 增加动态调参监控模块,可以在此基础上增加需要动态调整的功能模块

--- visualization_module/src/dynamic_visualization.cpp
+++ visualization_module/src/dynamic_visualization.cpp
@@ -38,81 +38,39 @@
 #include <dynamic_reconfigure/server.h>
 #include <visualization_module/VisualizationConfig.h>
 
-namespace VISUALIZATION_V1 {
-class monitor {
-private:
-    ros::NodeHandle nh;
+// namespace VISUALIZATION_DYNAMIC {
 
-    ros::Subscriber sub_carx_state;
-    // std::map<std::string, ros::Publisher> pub_current_pose_car;
-    // std::map<std::string, ros::Publisher> pub_current_text;
-    std::map<std::string, ros::Publisher> pub_current_state_array;
+ros::Subscriber sub_carx_state;
+std::map<std::string, ros::Publisher> pub_current_state_array;
+ros::Publisher pub_global_map;
+// tf::TransformBroadcaster tf_broadcaster_;  // 这玩意居然"也"是个 nodehandle ，如果在全局定义的话会报错：You must call ros::init() before creating the first NodeHandle
+tf::StampedTransform tf_base2laser_;
 
-    ros::Publisher pub_global_map;
-    tf::TransformBroadcaster tf_broadcaster_;
+double offset_x, offset_y, offset_z;
 
-    double offset_x, offset_y, offset_z;
+void load_map(ros::NodeHandle nh, std::string file);
 
-    void load_map(std::string file);
+void car_pose_cb(const monitor_msgs::PoseStateConstPtr msg);
 
-    void car_pose_cb(const monitor_msgs::PoseStateConstPtr msg);
+void pub_car_state_message(const monitor_msgs::PoseStateConstPtr msg);
 
-    void pub_car_state_message(const monitor_msgs::PoseStateConstPtr msg);
+visualization_msgs::Marker make_car_model(const monitor_msgs::PoseStateConstPtr msg);
 
-    visualization_msgs::Marker make_car_model(const monitor_msgs::PoseStateConstPtr msg);
+visualization_msgs::Marker make_car_message(const monitor_msgs::PoseStateConstPtr msg, double offset_x, double offset_y, double offset_z, std::string car_id);
 
-    visualization_msgs::Marker make_car_message(const monitor_msgs::PoseStateConstPtr msg, double offset_x, double offset_y, double offset_z);
+visualization_msgs::Marker make_line(geometry_msgs::Point start_point, double offset_x, double offset_y, double offset_z, std::string car_id);
 
-    visualization_msgs::Marker make_line(geometry_msgs::Point start_point, double offset_x, double offset_y, double offset_z);
+visualization_msgs::Marker car_model_initial();
 
-    visualization_msgs::Marker car_model_initial();
+visualization_msgs::Marker message_initial();
 
-    visualization_msgs::Marker message_initial();
-
-public:
-    monitor()
-    {
-        ros::NodeHandle pnh("~");
-
-        pnh.param<double>("offset_x", offset_x, 5.0);
-        pnh.param<double>("offset_y", offset_y, -5.0);
-        pnh.param<double>("offset_z", offset_z, 5.0);
-
-        sub_carx_state = nh.subscribe("/carx/current_state", 1, &monitor::car_pose_cb, this);
-        pub_global_map = nh.advertise<sensor_msgs::PointCloud2>("/map/global_map", 100);
-    };
-    ~monitor(){};
-
-    void run();
-};
-
-// void config_callback(visualization_module::VisualizationConfig& config, uint32_t level);
-}
-
-namespace VISUALIZATION_V1 {
-
-void monitor::run()
-{
-    ros::NodeHandle pnh("~");
-    std::string file;
-    pnh.param<std::string>("map_file", file, "");
-    load_map(file);
-
-    // dynamic_reconfigure::Server<visualization_module::VisualizationConfig> server;
-    // dynamic_reconfigure::Server<visualization_module::VisualizationConfig>::CallbackType cf;
-    // // cf = boost::bind(&monitor::config_callback, _1, _2);
-    // cf = boost::bind(&config_callback, this, _2);
-    // server.setCallback(cf);
-
-    ros::spin();
-}
-
-void monitor::load_map(std::string file)
+void load_map(ros::NodeHandle nh, std::string file)
 {
     if (file == "") {
         ROS_WARN_STREAM("No map file found!");
         return;
     }
+    pub_global_map = nh.advertise<sensor_msgs::PointCloud2>("/map/global_map", 100);
     sensor_msgs::PointCloud2::Ptr msg_globalmap(new sensor_msgs::PointCloud2());
     if (pcl::io::loadPCDFile(file, *msg_globalmap) == -1) {
         ROS_WARN_STREAM("Cannot load map");
@@ -124,12 +82,13 @@ void monitor::load_map(std::string file)
     std::cout << "Success load map: " << file << std::endl;
 }
 
-void monitor::car_pose_cb(const monitor_msgs::PoseStateConstPtr msg)
+void car_pose_cb(const monitor_msgs::PoseStateConstPtr msg)
 {
     visualization_msgs::MarkerArray marker_array;
     std::string car_vin = msg->vin;
     std::string car_id = car_vin.substr(13, 4);
     if (pub_current_state_array.find(car_id) == pub_current_state_array.end()) {
+        ros::NodeHandle nh;
         std::string topic = "/car" + car_id;
         ros::Publisher pub;
         pub = nh.advertise<visualization_msgs::MarkerArray>(topic, 10);
@@ -137,8 +96,8 @@ void monitor::car_pose_cb(const monitor_msgs::PoseStateConstPtr msg)
     }
     // car_model.header.stamp = msg->pose.header.stamp;
     visualization_msgs::Marker car_model = make_car_model(msg);
-    visualization_msgs::Marker car_message = make_car_message(msg, offset_x, offset_y, offset_z);
-    visualization_msgs::Marker car_infoline = make_line(msg->pose.pose.position, offset_x, offset_y, offset_z);
+    visualization_msgs::Marker car_message = make_car_message(msg, offset_x, offset_y, offset_z, car_id);
+    visualization_msgs::Marker car_infoline = make_line(msg->pose.pose.position, offset_x, offset_y, offset_z, car_id);
 
     marker_array.markers.push_back(car_model);
     marker_array.markers.push_back(car_message);
@@ -148,15 +107,17 @@ void monitor::car_pose_cb(const monitor_msgs::PoseStateConstPtr msg)
     it->second.publish(marker_array);
 }
 
-visualization_msgs::Marker monitor::make_car_model(const monitor_msgs::PoseStateConstPtr msg)
+visualization_msgs::Marker make_car_model(const monitor_msgs::PoseStateConstPtr msg)
 {
     visualization_msgs::Marker car_model = car_model_initial();
     car_model.header.stamp = ros::Time::now();
     car_model.pose.position = msg->pose.pose.position;
     std::string car_id = msg->vin.substr(13, 4);
+    car_model.ns = "/car" + car_id + "/model";
     // TODO: 区分虚拟车和实际车 --或使用不同的颜色,或使用不同的车型
 
     double current_roll, current_yaw, current_pitch;
+    tf::TransformBroadcaster tf_broadcaster_;
     tf::Quaternion quat;
     tf::quaternionMsgToTF(msg->pose.pose.orientation, quat);
     tf::Matrix3x3(quat).getRPY(current_roll, current_pitch, current_yaw);
@@ -169,7 +130,7 @@ visualization_msgs::Marker monitor::make_car_model(const monitor_msgs::PoseState
     return car_model;
 }
 
-visualization_msgs::Marker monitor::make_car_message(const monitor_msgs::PoseStateConstPtr msg, double offset_x, double offset_y, double offset_z)
+visualization_msgs::Marker make_car_message(const monitor_msgs::PoseStateConstPtr msg, double offset_x, double offset_y, double offset_z, std::string car_id)
 {
     visualization_msgs::Marker car_message = message_initial();
     if (int(msg->vin[12]) == 1) {
@@ -178,11 +139,12 @@ visualization_msgs::Marker monitor::make_car_message(const monitor_msgs::PoseSta
         car_message.color.g = 1;
     }
     car_message.header.stamp = ros::Time::now();
+    car_message.ns = "/car" + car_id + "/state";
 
     geometry_msgs::Point message_pose;
-    message_pose.x = msg->pose.pose.position.x + 5.0;
-    message_pose.y = msg->pose.pose.position.y - 5.0;
-    message_pose.z = msg->pose.pose.position.z + 2.0;
+    message_pose.x = msg->pose.pose.position.x + offset_x;
+    message_pose.y = msg->pose.pose.position.y + offset_y;
+    message_pose.z = msg->pose.pose.position.z + offset_z;
     car_message.pose.position = message_pose;
 
     std::stringstream text;
@@ -209,12 +171,12 @@ visualization_msgs::Marker monitor::make_car_message(const monitor_msgs::PoseSta
     return car_message;
 }
 
-visualization_msgs::Marker monitor::car_model_initial()
+visualization_msgs::Marker car_model_initial()
 {
     visualization_msgs::Marker marker_car;
     marker_car.header.frame_id = "map";
     marker_car.header.stamp = ros::Time::now();
-    marker_car.ns = "/car/model";
+    // marker_car.ns = "/car/model";
     marker_car.id = 0;
     marker_car.type = visualization_msgs::Marker::MESH_RESOURCE;
     marker_car.action = visualization_msgs::Marker::ADD;
@@ -231,26 +193,28 @@ visualization_msgs::Marker monitor::car_model_initial()
     return marker_car;
 }
 
-visualization_msgs::Marker monitor::message_initial()
+visualization_msgs::Marker message_initial()
 {
     visualization_msgs::Marker marker_message;
     marker_message.header.frame_id = "map";
-    marker_message.ns = "/car/state";
+    // marker_message.ns = "/car/state";
     marker_message.header.stamp = ros::Time::now();
     marker_message.action = visualization_msgs::Marker::ADD;
     marker_message.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
     marker_message.id = 0;
     marker_message.scale.z = 1.0;
-    marker_message.color.b = 1;
+    marker_message.color.r = 0.91;
+    marker_message.color.g = 0.585;
+    marker_message.color.b = 0.476;
     marker_message.color.a = 1;
     return marker_message;
 }
 
-visualization_msgs::Marker monitor::make_line(geometry_msgs::Point start_point, double offset_x, double offset_y, double offset_z)
+visualization_msgs::Marker make_line(geometry_msgs::Point start_point, double offset_x, double offset_y, double offset_z, std::string car_id)
 {
     visualization_msgs::Marker line;
     line.header.frame_id = "map";
-    line.ns = "/car/infoline";
+    line.ns = "/car" + car_id + "/infoline";
     line.header.stamp = ros::Time::now();
     line.action = visualization_msgs::Marker::ADD;
     line.type = visualization_msgs::Marker::LINE_STRIP;
@@ -268,18 +232,51 @@ visualization_msgs::Marker monitor::make_line(geometry_msgs::Point start_point, 
     return line;
 }
 
-// void monitor::config_callback(visualization_module::VisualizationConfig& config, uint32_t level)
-// {
-//     offset_x = config.offset_x;
-//     offset_y = config.offset_y;
-//     offset_z = config.offset_z;
-// }
+void config_callback(visualization_module::VisualizationConfig& config, uint32_t level)
+{
+    offset_x = config.offset_x;
+    offset_y = config.offset_y;
+    offset_z = config.offset_z;
 }
 
+// void run()
+// {
+//     param_initial();
+
+//     sub_carx_state = nh.subscribe("/carx/current_state", 1, &car_pose_cb);
+//     pub_global_map = nh.advertise<sensor_msgs::PointCloud2>("/map/global_map", 100);
+
+//     dynamic_reconfigure::Server<visualization_module::VisualizationConfig> server;
+//     dynamic_reconfigure::Server<visualization_module::VisualizationConfig>::CallbackType cf;
+//     cf = boost::bind(&config_callback, _1, _2);
+//     server.setCallback(cf);
+
+//     ros::spin();
+// }
+// }
+
+// using namespace VISUALIZATION_DYNAMIC;
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "visualization_v1");
-    VISUALIZATION_V1::monitor app;
-    app.run();
+    ros::init(argc, argv, "visualization_module");
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh("~");
+
+    pnh.param<double>("offset_x", offset_x, 5.0);
+    pnh.param<double>("offset_y", offset_y, -5.0);
+    pnh.param<double>("offset_z", offset_z, 5.0);
+
+    std::string file;
+    pnh.param<std::string>("map_file", file, "");
+    load_map(nh, file);
+
+    sub_carx_state = nh.subscribe("/carx/current_state", 1, car_pose_cb);
+
+    dynamic_reconfigure::Server<visualization_module::VisualizationConfig> server;
+    dynamic_reconfigure::Server<visualization_module::VisualizationConfig>::CallbackType cf;
+    cf = boost::bind(&config_callback, _1, _2);
+    server.setCallback(cf);
+
+    ros::spin();
     return 0;
 }
